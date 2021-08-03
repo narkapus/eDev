6db092cf3f97080ec8322e7ca0007c3d81242a7c
peeya <peeya.sintu@gmail.com>
2021-08-03 11:37:36 +0700

    +fix css&js dataTable

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,14 +3,23 @@
 @section('content')
 
 <!-- <script src="{{ asset('material') }}/js/plugins/jquery-2.2.4.min.js"></script>  -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+    <link href="{{ asset('material') }}/css/dataTables/buttons.dataTables.min.css" rel="stylesheet">
+    <link href="{{ asset('material') }}/css/dataTables/jquery.dataTables.min.css" rel="stylesheet">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> --}}
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> -->
+    <script src="{{ asset('material') }}/js/core/jquery-3.5.1.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/jszip.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/pdfmake.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/vfs_fonts.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/buttons.html5.min.js"></script>
 <?php
 $date = Carbon\Carbon::now();
 ?>
@@ -91,9 +100,9 @@ $date = Carbon\Carbon::now();
                     <div class="form-group">
                         <label>ประเภทเอกสาร</label>
                         <div class="col-md-6">
-                            <input class="form-control" id="filename" name='filename' type="text" style="readonly:true;">
                         </div>
                         <div class="col-md-4">
+                            <input class="form-control" id="filename" name='filename' type="text" readOnly>
                             <label for="eFile" class="custom-file-upload">
                                 <i class="fa fa-cloud-upload"></i> Upload file
                             </label>
