26be662ce87ea1c08129e14e6f61fe2c771ac3d5
Mick <narkapus@gmail.com>
2021-08-11 00:29:14 +0700

    ++

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,14 +3,23 @@
 @section('content')
 
 <!-- <script src="{{ asset('material') }}/js/plugins/jquery-2.2.4.min.js"></script>  -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+    <link href="{{ asset('material') }}/css/dataTables/buttons.dataTables.min.css" rel="stylesheet">
+    <link href="{{ asset('material') }}/css/dataTables/jquery.dataTables.min.css" rel="stylesheet">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> --}}
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script> -->
+    <script src="{{ asset('material') }}/js/core/jquery-3.5.1.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/jszip.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/pdfmake.min.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/vfs_fonts.js"></script>
+    <script src="{{ asset('material') }}/js/plugins/buttons.html5.min.js"></script>
 <?php
 $date = Carbon\Carbon::now();
 ?>
@@ -44,10 +53,11 @@ $date = Carbon\Carbon::now();
                   <tr>
                     <td style="width:10%">ลำดับ</td>
                     <td style="width:25%">ประเภทเอกสาร</td>
-                    <td style="width:25%">เจ้าของเอกสาร</td>
                     <td style="width:15%">เอกสาร</td>
+                    <td style="width:15%">ผู้บันทึก</td>
                     <td style="width:10%">บันทึกวันที่</td>
                     <td style="width:10%">แก้ไขวันที่</td>
+                    <td style="width:10%">Action</td>
                   </tr>
                 </thead>
                 <tbody>
@@ -66,7 +76,7 @@ $date = Carbon\Carbon::now();
         <div class="modal-content">
             <form  class="form-submit" id="addFormDoc" method="post"  action="{{ url('/home/save') }}" enctype="multipart/form-data">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">เพิ่มเอกสาร</h5>
+                    <h4 class="modal-title" id="docModal"></h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -90,9 +100,9 @@ $date = Carbon\Carbon::now();
                     <div class="form-group">
                         <label>ประเภทเอกสาร</label>
                         <div class="col-md-6">
-                            <input class="form-control" id="filename" name='filename' type="text" style="readonly:true;">
                         </div>
                         <div class="col-md-4">
+                            <input class="form-control" id="filename" name='filename' type="text" readOnly>
                             <label for="eFile" class="custom-file-upload">
                                 <i class="fa fa-cloud-upload"></i> Upload file
                             </label>
@@ -113,6 +123,7 @@ var table = document.getElementById('addDocument');
 function addDocument(){
     $('#modalAddDocument').modal('show');
         $('#addFormDoc')[0].reset();
+        $('#docModal').html("เพิ่มเอกสาร");
         $('#action').val('Add');
 }
 $('#eFile').change(function() {
@@ -126,18 +137,13 @@ $(function () {
       serverSide: true,
         ajax: "{{ route('home') }}",
         columns: [
-            {data: 'eCode', name: 'eCode'},
-            {data: 'eName', name: 'eName'},
-            { "data": "eFile", "name": "eFile",
-                fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
-                    // if(oData.client_name) {
-                    $(nTd).html("<a href='"+sData+"'>"+sData+"</a>");
-                    // }
-                }
-            },
+            {data: "DT_RowIndex" },
+            {data: 'mdName', name: 'mdName'},
+            {data: 'eFile', name: 'eFile'},
             {data: 'name', name: 'name'},
-            {data: 'created_at', name: 'created_at', orderable: true, searchable: true},
-            {data: 'created_at', name: 'created_at', orderable: true, searchable: true},
+            {data: 'created_at', name: 'created_at'},
+            {data: 'updated_at', name: 'updated_at'},
+            {data: 'action', name: 'action'},
         ]
   });
 
